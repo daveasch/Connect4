cdc1e871bc9e668ea604809d21e83445c9aa2ae9
daveasch <daveasch@umich.edu>
2017-04-11 19:05:17 -0400

    Big Changes: 1/4 Done

--- graphics.cpp
+++ graphics.cpp
@@ -17,7 +17,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define _USE_MATH_DEFINES
-#include <cmath>
+#include <cmath> 
 #include <string>
 #include "graphics.h"
 #include "connect4.h"
@@ -46,12 +46,12 @@ int wd;
 Board board;
 Result r;
 
-void init(void)
-{
+void init(void) {
     // initial window width and height,
     // within which we draw. (0,0) is the lower left corner
     width  = 600.0;
     height = 600.0;
+
 
     mouseX = 0;
     mouseY = 0;
@@ -90,18 +90,27 @@ void display(void)
     glClear(GL_COLOR_BUFFER_BIT);
   
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-    
-    glColor3f(1.0, 1.0, 0.0);
+   
+	glColor3f(0.0, 0.0, 0.0);
+	glBegin(GL_QUADS);
+	glVertex2i(width, 0);
+	glVertex2i(width, height);
+	glVertex2i(0, height);
+	glVertex2i(0, 0);
+	glEnd();
+
+    glColor3f(0.5, 0.5, 0.5);
     glBegin(GL_QUADS);
     glVertex2i(side_border,bottom_border);
     glVertex2i(width-side_border,bottom_border);
-    glVertex2i(width-side_border,height-top_border);
-    glVertex2i(side_border,height-top_border);
+    glVertex2i(width-side_border,width-top_border);
+    glVertex2i(side_border,width-top_border);
     glEnd();
     
     // Begin solution
     
     glColor3f(1.0, 1.0, 1.0);
+	const int numCols = 7;
     int increment = 75;
     int radius = 25;
     for (int row = 1; row <= NUM_ROWS; ++row) {
@@ -110,14 +119,14 @@ void display(void)
             if (location == Empty) {
                 glColor3f(1.0, 1.0, 1.0);
             } else if (location == Player1) {
-                glColor3f(0.0, 0.0, 0.0);
+                glColor3f(0.0, 1.0, 1.0);
             } else {
                 glColor3f(1.0, 0.0, 0.0);
             }
             drawCircle((col*increment), side_border+(row*increment), radius);
         }
     }
-    glColor3f(0.0, 0.0, 0.0);
+    glColor3f(1.0, 0.0, 0.0);
     for (int col = 1; col <= NUM_COLS; ++col) {
         glRasterPos2i(col*increment-5, 20);
         glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, char('0'+col));
@@ -126,28 +135,28 @@ void display(void)
     if(r == Win){
         std::string win_message;
         if(board.toMove() == 1) {
-            win_message = "Red won!";
+            win_message = "\"You underestimate my power!\" --- Anakin won!";
             glColor3f(1.0, 0.0, 0.0);
         }
         else {
-            win_message = "Black won!";
-            glColor3f(0.0, 0.0, 0.0);
+			win_message = "\"It's over, Anakin; I have the high ground.\" --- Obi-Wan won!";
+            glColor3f(0.0, 1.0, 1.0);
         }
         glRasterPos2i(side_border, height-side_border);
         for (int s = 0; s < win_message.length(); ++s) {
             glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, win_message[s]);
         }
     } else if (r == Draw) {
-        std::string draw = "Draw!";
-        glColor3f(0.5, 0.0, 0.0);
+        std::string draw = "Draw! Meesa getting very very scared!";
+        glColor3f(1.0, 1.0, 0.0);
         glRasterPos2i(side_border, height-side_border);
         for (int s = 0; s < draw.length(); ++s) {
             glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, draw[s]);
         }
     } else if (board.toMove() == 1) {
-        glColor3f(0.0, 0.0, 0.0);
+        glColor3f(0.0, 1.0, 1.0);
         glRasterPos2i(side_border, height-side_border);
-        std::string message = "Black, enter a move.";
+        std::string message = "Obi-Wan, enter a move.";
         for (int s = 0; s < message.length(); ++s) {
             glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, message[s]);
         }
@@ -155,15 +164,15 @@ void display(void)
     } else {
         glColor3f(1.0, 0.0, 0.0);
         glRasterPos2i(side_border, height-side_border);
-        std::string message = "Red, enter a move.";
+        std::string message = "Ani, enter a move.";
         for (int s = 0; s < message.length(); ++s) {
             glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, message[s]);
         }
     }
     
     if (r == IllegalMove) {
-        std::string draw = "Illegal Move!";
-        glColor3f(0.5, 0.0, 0.0);
+        std::string draw = "Illegal Move! I don't care which galaxy you're from, that's gotta hurt!";
+        glColor3f(1.0, 1.0, 1.0);
         glRasterPos2i(side_border, height-bottom_border);
         for (int s = 0; s < draw.length(); ++s) {
             glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, draw[s]);
@@ -211,26 +220,53 @@ void kbd(unsigned char key, int x, int y)
     return;
 }
 
-void cursor(int x, int y)
-{
-
+void cursor(int x, int y){
+	// Account for reversal of y coordinates through new coord system
+	mouseY = height - y;
+	// print a token onto the cursor
+	x = mouseX;
+	y = mouseY;
+	const int RADIUS = 25;
+	drawCircle(x, y, RADIUS);
+	// If clicked down, switch color????
     glutPostRedisplay();
-
     return;
 }
 
-void drag(int x, int y)
-{
+void drag(int x, int y){
     glutPostRedisplay();
-
     return;
 }
 
-void mouse(int button, int state, int x, int y)
-{
-    glutPostRedisplay();
+void mouse(int button, int state, int x, int y) {
+	// Iterate through columns till column of click is found
+	int minPixel = 37.5;
+	int maxPixel = 112.5;
+	int increment = 75;
+	if (r != Win && r != Draw) {
+		if (state == GLUT_DOWN) {
+			if (y < 550) {
+				for (int i = 0; i < NUM_COLS; i++) {
+					if ((x >= minPixel) && (x <= maxPixel)) {
+						r = board.makeMove(i);
+						return;
+					}
+					minPixel += increment;
+					maxPixel += increment;
+				}
+			}
+			/*if (y > 550) {
+				for (int i = 0; i < NUM_COLS; i++) {
+					if ((x >= minPixel) && (x <= maxPixel)) {
+						.// Dropping animation goes here;
 
-    return;
+			}
+			r = IllegalMove;
+			*/
+			glutPostRedisplay();
+			return;
+		}
+	}
 }
 
 int graphicsPlay(int argc, char *argv[])
